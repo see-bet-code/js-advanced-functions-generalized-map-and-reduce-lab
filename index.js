3125218d68506def48b47180f5d5aec9dc0f97a1
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-02 09:16:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, f) {
+    const res = []
+    for (const s of src) {
+        res.push(f(s))
+    }
+    return res
+}
+
+function reduce(src, f, startVal) {
+    let r = (startVal) ? startVal : undefined
+    src.forEach((s, i) => {
+        if (r === undefined && i === 0) {
+            r = s
+        } else {
+            r = f(s, r)
+        } 
+    })
+    return r
+}
